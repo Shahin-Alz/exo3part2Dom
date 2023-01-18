fdfe5e23ffe47ffda52dc352e8bd7dd0df014e39
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-18 15:40:15 +0100

    finish

---
+++ main.js
@@ -0,0 +1,43 @@
+// let recup = document.querySelector("div");
+// console.log(recup);
+
+///////////////////
+
+// let recup = document.querySelectorAll("div")[0];
+// console.log(recup);
+
+///////////////////
+
+// let recup = document.querySelector(".important");
+// console.log(recup);
+
+///////////////////
+
+// let recup = document.querySelectorAll(".important");
+// console.log(recup);
+
+//////////////////
+
+// let recup = document.querySelectorAll("li");
+// recup.forEach(recup => {
+    
+//     console.log(recup.textContent.toUpperCase());
+// });
+
+/////
+
+// let recup = document.querySelectorAll("li");
+
+// recup.forEach.call(recup, function(li) {
+//     let lastLetter = li.textContent.charAt(li.textContent.length - 1);
+//     let newString = li.textContent.slice(0, -1) + lastLetter.toUpperCase();
+//     console.log(newString);
+// });
+
+//////////////////////
+
+// let divId = document.getElementById("content");
+// let para = document.getElementsByClassName("grandParagraphe");
+
+// console.log(divId);
+// console.log(para);
